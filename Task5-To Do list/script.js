bd8efb8e9f0cf7a00348d5b765e32588f94823c7
M.Deepa <deepamarimuthu99@gmail.com>
2026-06-06 20:48:29 +0530

    Task5-To Do List

---
+++ Task5-To Do list/script.js
@@ -0,0 +1,37 @@
+function addTask(){
+
+    let input = document.getElementById("taskInput");
+
+    let task = input.value.trim();
+
+    if(task === ""){
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+        <span class="task-text" onclick="toggleTask(this)">
+            ${task}
+        </span>
+
+        <button class="delete-btn" onclick="deleteTask(this)">
+            Delete
+        </button>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
+
+function deleteTask(button){
+
+    button.parentElement.remove();
+}
+
+function toggleTask(task){
+
+    task.classList.toggle("completed");
+}
